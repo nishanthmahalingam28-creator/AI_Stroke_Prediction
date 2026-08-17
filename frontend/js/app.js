c2a2d2f6a5eb07510f81552b939ff93fba6fdb93
Nishanth Mahalingam <nishanthmahalingam28@gmail.com>
2026-08-17 11:42:30 +0530

    Updated

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -17,7 +17,7 @@
   // file hardcodes the host/port or the endpoint path.
   // ---------------------------------------------------------------
   var CONFIG = {
-    API_BASE_URL: "http://127.0.0.1:5000",
+    API_BASE_URL: "",
     PREDICT_ENDPOINT: "/api/predict",
   };
 
